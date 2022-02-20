ee39ebf7cdcc635e0f91b14b09888c68a46a1d9b
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2022-02-20 15:55:55 +0530

    my-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import RouterPractice from './RouterPractice';
+import Functioninput from './Component/Functioninput';
 
 function App() {
   return (
     <div className="App">
      <RouterPractice/>
+     <Functioninput/>
     </div>
   );
 }
