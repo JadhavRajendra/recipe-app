e502995c8f00b3f80e18b04741614730e05daa95
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2022-03-01 14:02:47 +0530

    Update FunctionExzample.jsx

--- my-app/src/Component/FunctionExzample.jsx
+++ my-app/src/Component/FunctionExzample.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FunctionExzample() {
   return (
     <div>
-      <h1> hallo</h1>
+      <h1> hallo word</h1>
       <input type="range"></input>
     </div>
   )
