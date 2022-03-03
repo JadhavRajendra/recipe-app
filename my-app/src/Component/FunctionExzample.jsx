d83afcc0013b2298e8e2fa20b553e42ac6eddef2
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2022-03-03 16:07:56 +0530

    functionexzample

--- my-app/src/Component/FunctionExzample.jsx
+++ my-app/src/Component/FunctionExzample.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
 export default function FunctionExzample() {
   return (
-    <div>
+    <div className='my-image'>
       <h1> hallo word</h1>
-      <input type="range"></input>
+      <input type="range"></input><br></br>
+      <br></br>
+      <input type="email"></input><br></br>
+      <image src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"></image>
+      <br></br>
+      <input type="password"></input><br></br>
+      <br></br>
+
+         <input type="datetime-local"></input>
+
+      <br></br>
+      <input type="button"></input>
+      <br></br>
+
+      <input type="file"></input><br></br>
+
+
     </div>
   )
-}
+} 
