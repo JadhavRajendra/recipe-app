412abbc7d66bd94c9eb25252d3335a063b08d813
JadhavRajendra <92634415+JadhavRajendra@users.noreply.github.com>
2022-03-04 16:04:46 +0530

    FUNCTIONEZXAMPLE

--- my-app/src/Component/FunctionExzample.jsx
+++ my-app/src/Component/FunctionExzample.jsx
@@ -3,10 +3,11 @@ export default function FunctionExzample() {
   return (
     
       
-      <div class="row">
+      <div class="row" >
   <div class="col">
     <input type="text" class="form-control" placeholder="First name" aria-label="First name"></input>
   </div>
+  <br></br>
   <div class="col">
     <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"></input>
   </div>
